e37498a79b8882d76ccd2621320f6df7437eb5d8
LairBob <laurent@stanevich.com>
2011-03-04 18:01:29 -0500

    - Basic "HTML include" shortcode is working

--- plugins/essayist/essayist-tools-html-inc.php
+++ plugins/essayist/essayist-tools-html-inc.php
@@ -1,7 +1,17 @@
 <?php
+
 add_shortcode('html-inc', 'htmlInclude');
 
-function htmlInclude() {
-    
+function htmlInclude($atts) {
+    extract(shortcode_atts(array(
+        "incfile" => 'test.html',
+         "incdir" => 'test',
+     ), $atts));
+
+    // TODO Add error-checking to filepath / filepath strings
+    $strIncFile = dirname(__FILE__) . '/includes/' . $incdir . '/' . $incfile;
+
+    return file_get_contents($strIncFile);
 }
+
 ?>
