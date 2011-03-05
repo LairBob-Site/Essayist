677ecadf2851fd93435f1badb200fb9b39ea0307
LairBob <laurent@stanevich.com>
2011-03-04 20:21:48 -0500

    - Added (momentarily meaningless) loop to explode and re-assemble the include text

--- plugins/essayist/essayist-tools-html-inc.php
+++ plugins/essayist/essayist-tools-html-inc.php
@@ -4,14 +4,26 @@ add_shortcode('html-inc', 'htmlInclude');
 
 function htmlInclude($atts) {
     extract(shortcode_atts(array(
-        "incfile" => 'test.html',
-         "incdir" => 'test',
-     ), $atts));
+                "incfile" => 'test.html',
+                "incdir" => 'test',
+                    ), $atts));
 
     // TODO Add error-checking to filepath / filepath strings
-    $strIncFile = dirname(__FILE__) . '/includes/' . $incdir . '/' . $incfile;
+    $strIncFileName = dirname(__FILE__) . '/includes/' . $incdir . '/' . $incfile;
 
-    return file_get_contents($strIncFile);
+    $strIncFile = file_get_contents($strIncFileName);
+
+    // Explode the include string into an array of individual lines
+    $expIncFile = explode("\n", $strIncFile);
+
+    // TODO Add substitution logic for "slider" shortcode
+    // Reassemble the array of lines back into a single string, eventually applying custom logic
+    $outIncFile = "";
+    for ($i = 0; $i < count($expIncFile); $i++) {
+        $outIncFile = $outIncFile . "\n" . $expIncFile[$i]; //write value by index
+    }
+
+    return $outIncFile;
 }
 
 ?>
