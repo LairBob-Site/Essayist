e37acd7ea3c643742d87bc3372240dd05362e1c7
LairBob <laurent@stanevich.com>
2011-03-06 22:02:12 -0500

    - Fixed entry image styles to automatically crop images that are wider than the current entry panel width - Tweaked main.css styles so that the thumbnails didn't break any more due to that change

--- plugins/essayist/essayist-tools-html-inc.php
+++ plugins/essayist/essayist-tools-html-inc.php
@@ -12,12 +12,14 @@ function htmlInclude($atts) {
     $strIncFileName = dirname(__FILE__) . '/includes/' . $incdir . '/' . $incfile;
 
     $strIncFile = file_get_contents($strIncFileName);
+
+    // Apply any other shortcodes that are embedded within the content
     $strIncFile = do_shortcode($strIncFile);
+    // $strIncFile = wpautop($strIncFile);
 
     // Explode the include string into an array of individual lines
     $expIncFile = explode("\n", $strIncFile);
 
-    // TODO Add substitution logic for "slider" shortcode
     // Reassemble the array of lines back into a single string, eventually applying custom logic
     $outIncFile = "";
     for ($i = 0; $i < count($expIncFile); $i++) {
