9075eeece66735d77637a6a9c9b54205cd6e2b39
LairBob <laurent@stanevich.com>
2011-03-11 14:36:16 -0500

    - Commit after moving files into a more organized directory structure

--- themes/essayist/library/js/entry-nav.js
+++ themes/essayist/library/js/entry-nav.js
@@ -3,19 +3,20 @@ jQuery(document).ready(function ($) {
     // $(primary).css('top', $(section-01).offset().top);
     // var primaryTop = jQuery(document).(primary);
     $('#primary').localScroll();
-    alert("Here");
+    // alert("Here");
 
     $(window).scroll(function(){
-        var objectTop = $('#primary').position().top;
-        var contentTop = $('.entry-content').position().top;
+        var objectTop = $('#primary').offset().top;
+        var contentTop = $('#content').offset().top;
         var objectHeight = $('#primary').outerHeight();
         var windowScrollTop = $(window).scrollTop();
         var windowHeight = $(window).height();
 
-        if  (windowScrollTop  > objectTop)
+        if  (windowScrollTop  > contentTop)
             $('#primary').css('top', windowScrollTop );
-        else if ((windowScrollTop+windowHeight) < (objectTop + objectHeight))
-            $('#primary').css('top', (windowScrollTop+windowHeight) - objectHeight);
+        else $('#primary').css('top', contentTop );
+        // else if ((windowScrollTop+windowHeight) < (objectTop + objectHeight))
+            // $('#primary').css('top', (windowScrollTop+windowHeight) - objectHeight);
 
         // $('#primary').html('Top: ' + $('#primary').position().top + 'px');
 
