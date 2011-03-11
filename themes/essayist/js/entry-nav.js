d12eae6b2615852b37b6a6b2f177a2a32f20829f
LairBob <laurent@stanevich.com>
2011-03-11 11:05:56 -0500

    - Added code from Stack Overflow question to successfully handle "bounded scrolling" - About to try and tweak the behavior so that the box just stops at the top

--- themes/essayist/js/entry-nav.js
+++ themes/essayist/js/entry-nav.js
@@ -1,6 +1,25 @@
 // function lockSectionNav() {
-    jQuery(document).ready(function($){
-        $(navBlock).css('top', $(navAnchor).offset().top);
-        // alert("Yo");
+jQuery(document).ready(function ($) {
+    // $(primary).css('top', $(section-01).offset().top);
+    // var primaryTop = jQuery(document).(primary);
+    $('#primary').localScroll();
+    alert("Here");
+
+    $(window).scroll(function(){
+        var objectTop = $('#primary').position().top;
+        var contentTop = $('.entry-content').position().top;
+        var objectHeight = $('#primary').outerHeight();
+        var windowScrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if  (windowScrollTop  > objectTop)
+            $('#primary').css('top', windowScrollTop );
+        else if ((windowScrollTop+windowHeight) < (objectTop + objectHeight))
+            $('#primary').css('top', (windowScrollTop+windowHeight) - objectHeight);
+
+        // $('#primary').html('Top: ' + $('#primary').position().top + 'px');
+
     });
+})
+
 // }
