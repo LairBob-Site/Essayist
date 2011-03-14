4a4387b821dd443488f4c4c187517c3f81deeb57
LairBob <laurent@stanevich.com>
2011-03-14 14:38:07 -0400

    - Floating / locked menu bar now works correctly

--- themes/essayist/library/js/entry-nav.js
+++ themes/essayist/library/js/entry-nav.js
@@ -10,19 +10,27 @@ jQuery(document).ready(function ($) {
         $.scrollTo(
             destAnchor,
             750,
-            { easing: 'easeOutQuint'}
+            { easing: 'easeOutQuint',
+              offset: -50
+            }
         );
     })
 
     var offNav = $('#primary').offset();
-    var offContent = $('#entry-content').offset();
+    var offMenu = $('#menu-all-navigation').offset();
 
     $(window).scroll(function () {
         var scrollTop = $(window).scrollTop(); // check the visible top of the browser
 
-        if (offNav.top<scrollTop) {
-            // $('#primary').css( {top : offContent.top} );
+        // The number subtracted here will be the amount that the nav widget is positioned down from the top of the window
+        // It needs to match the 'top' value defined in the corresponding 'fixed-nav' CSS style
+        if ((offNav.top - 50) <scrollTop) {
             $('#primary').addClass('fixed-nav');
         } else $('#primary').removeClass('fixed-nav');
+
+        if ((offMenu.top - 0) <scrollTop) {
+            $('#menu-all-navigation').addClass('fixed-menu');
+        } else $('#menu-all-navigation').removeClass('fixed-menu');
+
     });
 })
